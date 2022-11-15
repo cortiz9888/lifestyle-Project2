64e78e7dd5271da998be642122dc59153bbdd88f
Cortiz9888 <cortiz9888@gmail.com>
2022-11-15 16:10:03 -0600

    issues with directly calling within API

--- lifestyle-project2/src/App.js
+++ lifestyle-project2/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import SearchBar from "./Components/SearchBar/SearchBar"
+import WorkoutData from "./WorkoutData.json"
+
 
 function App() {
+  const APICall = () =>{
+    fetch ('https://wger.de/api/v2/')
+  
+  .then(res => {console.log(res.json())})
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <button onClick = {APICall}>button</button>
+       <SearchBar data={WorkoutData.json}/> 
     </div>
   );
 }
 
 export default App;
+
+//cc0113a06f599c529f0a7d4ca591fbf99c233c9a
